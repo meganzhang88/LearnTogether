a11c5b0a7c14d66383cef646c642c09e408097be
Kiley Supina <kileyallison@kileys-mbp.wireless-1x.unc.edu>
2019-02-16 22:52:52 -0500

    help

--- public/mainbackup.ts
+++ public/mainbackup.ts
@@ -1,3 +1,25 @@
+import * as firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/firestore';
+
+/**
+ * Initialize Firebase
+ *
+ * NOTE: This will be publically visible
+ * please don't accidentally publish your
+ * actual production credentials here.
+ * Create a sample project that mimics
+ * your setup to reproduce the error.
+ */
+
+
+// Reproduce Issue below
+// ...
+
+export const auth = firebase.auth();
+export const database = firebase.database();
+
 var firstName = document.getElementById("fname-input") as HTMLElement;
 var lastName = document.getElementById("lname-input") as HTMLElement;
 var department = document.getElementById("dept") as HTMLElement;
@@ -14,6 +36,7 @@ class User {
     courseNumber: number;
     latitude: number;
     longitude: number;
+    distance: number;
 
     constructor(firstName: string, lastName: string, department: string, courseNumber: number, latitude: number, longitude: number) {
         this.firstName = firstName;
@@ -22,9 +45,17 @@ class User {
         this.courseNumber = courseNumber;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.distance = 0;
     }
 }
 
 finalNextButton.onclick = (event: MouseEvent) => {
     let currentUser = new User(firstName.innerText, lastName.innerText, department.innerText, parseInt(courseNumber.innerText), parseInt(latitude.innerText), parseInt(longitude.innerText));
 }
+
+let getDistance = (lat1: number, lat2: number, lng1: number, lng2: number): number => {
+    let latDiff : number = Math.abs(lat1 - lat2);
+    let lngDiff : number = Math.abs(lng1 - lng2);
+    return Math.sqrt(latDiff + lngDiff);
+}
+
